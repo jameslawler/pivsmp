0767a219ada8a8d9f4552d6f104ab7957a0d95d3
jameslawler <services@jameslawler.com>
2021-01-01 16:33:45 +0100

    Handle no movie on status

--- src/status.py
+++ src/status.py
@@ -3,7 +3,7 @@ import display
 
 print("Display: " + config.getDisplayConfig() + "(" + str(display.getWidth()) + "x" + str(display.getHeight()) + ")")
 print("-----------------------------")
-print("Movie: " + config.getMovieConfig())
+print("Movie: " + (config.getMovieConfig() or "<None Selected>"))
 print("Position: " + config.getPositionConfig())
 print("Delay: " + config.getDelayConfig() + " seconds")
 print("-----------------------------")
