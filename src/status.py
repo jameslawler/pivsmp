76347542d11c3dd6cfd00ea052bcfa90037a8d45
jameslawler <services@jameslawler.com>
2021-01-01 16:32:11 +0100

    Convert int to string for status

--- src/status.py
+++ src/status.py
@@ -1,7 +1,7 @@
 import config
 import display
 
-print("Display: " + config.getDisplayConfig() + "(" + display.getWidth() + "x" + display.getHeight() + ")")
+print("Display: " + config.getDisplayConfig() + "(" + str(display.getWidth()) + "x" + str(display.getHeight()) + ")")
 print("-----------------------------")
 print("Movie: " + config.getMovieConfig())
 print("Position: " + config.getPositionConfig())
