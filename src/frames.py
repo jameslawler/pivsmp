19836c8b747756b5a62ceafac456f67e3903d0c6
jameslawler <services@jameslawler.com>
2021-02-17 21:22:37 +0100

    Add video support

--- src/frames.py
+++ src/frames.py
@@ -9,17 +9,50 @@
 
 import os
 from PIL import Image
+import ffmpeg
 
 import movies
 
-def getFrameImage(movie, position):
-  moviePath = movies.getMoviePath(movie)
+def getFrameFromVideo(movieFolder, position):
+  movieFilePath = movieFolder + "/" + "movie.mp4"
+  imageFilePath = movieFolder + "/" + "frame.bmp"
 
-  imageFilePath = moviePath + "/" + str(position).zfill(4) + ".png"
+  time = "%dms"%(position*41.666666)
+
+  try:
+      ffmpeg
+        .input(movieFilePath, ss=time)
+        .filter("scale", "iw*sar", "ih")
+        .filter("scale", width, height, force_original_aspect_ratio=1)
+        .filter("pad", width, height, -1, -1)
+        .output(imageFilePath, vframes=1)
+        .overwrite_output()
+        .run(capture_stdout=True, capture_stderr=True)
+    except ffmpeg.Error as e:
+        print('stdout:', e.stdout.decode('utf8'))
+        print('stderr:', e.stderr.decode('utf8'))
+        raise e
+
+  return imageFilePath
+
+def getFrameFromImage(movie, position):
+  movieFilePath = movies.getMoviePath(movie)
+
+  imageFilePath = movieFilePath + "/" + str(position).zfill(4) + ".png"
 
   if not os.path.exists(imageFilePath):
     print("File path does not exist: " + imageFilePath)
     return None
+
+  return imageFilePath
+
+def getFrameImage(movie, position):
+  movieFilePath = movies.getMoviePath(movie) + "/" + "movie.mp4"
+
+  if not os.path.exists(movieFilePath):
+    imageFilePath = getFrameFromImage(movie, position)
+  else
+    imageFilePath = getFrameFromVideo(movie, position)
 
   image = Image.open(imageFilePath)
   
